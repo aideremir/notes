23e097f3e2e5035e2d3f08c833df570012c6fd46
Aider Emirsuinov <aideremir@gmail.com>
2023-10-16 16:54:40 +0300

    chore: add prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
+  plugins: [],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, endOfLine: 'auto', printWidth: 120, semi: true, trailingComma: 'all', tabWidth: 2 },
+    ],
+  },
+}
