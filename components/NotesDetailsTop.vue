8d933286798c764affe1a681b69ec2785aac84be
Aider Emirsuinov <aideremir@gmail.com>
2023-10-19 01:29:24 +0300

    chore: add some components

---
+++ components/NotesDetailsTop.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="notes-details-top">
+    <div class="notes-details-top__menu">
+      <button v-if="props.isEdit" @click="toggleEditHandler">M↓</button>
+      <button v-else @click="toggleEditHandler">edit</button>
+
+      <NotesSearchForm @submit="searchHandler" />
+    </div>
+    <div class="notes-details-top__title">
+      {{ props.title }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineEmits } from 'vue';
+import NotesSearchForm from '~/components/NotesSearchForm.vue';
+
+const emit = defineEmits(['search', 'toggle-edit']);
+
+const props = defineProps({
+  isEdit: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+});
+
+const searchHandler = (query: string) => {
+  emit('search', { query });
+};
+
+const toggleEditHandler = () => {
+  emit('toggle-edit');
+};
+</script>
+
+<style scoped></style>
---
+++ components/NotesListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="notes-list-item">
+    <div>{{ props.note.title }}</div>
+    <div>{{ props.note.createdAt }}</div>
+    <div>{{ props.note.content || 'No additional text' }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue';
+import { INote } from '~/types';
+
+const props = defineProps({
+  note: {
+    type: Object as PropType<INote>,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
