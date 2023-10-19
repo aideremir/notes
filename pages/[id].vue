282e86a3dc34840961049685b359aa0c4c609319
Aider Emirsuinov <aideremir@gmail.com>
2023-10-19 03:36:16 +0300

    feat: add markdown parser

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import VueMarkdown from 'vue-markdown-render';
 import { useNotesStorage } from '~/composables/useNotesStorage';
 
 const { currentNote, notesState } = useNotesStorage();
@@ -8,7 +9,7 @@ notesState.value.isEdit = false;
 <template>
   <div v-if="currentNote" class="note-details">
     <h1>{{ currentNote.title }}</h1>
-    <p>{{ currentNote.content }}</p>
+    <vue-markdown :source="currentNote.content" />
   </div>
 </template>
 
